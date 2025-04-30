e57063c41c31a8b151bac3c71de8c94609e27ee6
Ryan <ryan@persist-ai.com>
2025-04-29 21:36:33 -0700

    api connection successful

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
 from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
+from flask import Flask, render_template
+import requests
 
 # Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 
+nyt_api_key = os.getenv('NYT_API_KEY')
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    stories = get_stories()
+
+    print(stories)
+
+
+    return render_template('index.html', stories = stories)
+
+
+def get_stories(): 
+    url = 'https://api.nytimes.com/svc/topstories/v2/home.json'
+    params = {'api-key': nyt_api_key}
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        return response.json()['results']
+    else:
+        return []
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
