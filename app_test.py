252c9693f4feffe4f69427304948438443371ff1
Ryan <ryan@persist-ai.com>
2025-04-30 19:19:09 -0700

    made testing more comprehensive for extract_city_from_keywords function

--- app_test.py
+++ app_test.py
@@ -1,8 +1,27 @@
 import app
 from unittest.mock import patch, Mock
 
-def test_extract_city_from_keywords():
+
+#Comprehensive testing for extract_city_from_keywords function
+def test_extract_city_from_keywords_sacramento():
     assert app.extract_city_from_keywords([{"value": "Sacramento"}]) == "Sacramento"
+
+def test_extract_city_from_keywords_davis(): 
+    assert app.extract_city_from_keywords([{"value": "City of Davis"}]) == "Davis"
+
+def test_extract_city_from_keywords_none(): 
+    assert app.extract_city_from_keywords([{"value": "Los Angeles"}]) is None
+
+def test_extract_city_from_keywords_empty(): 
+    assert app.extract_city_from_keywords([{"value": ""}]) is None
+
+def test_extract_city_from_keywords_case_insensitive(): 
+    assert app.extract_city_from_keywords([{"value": "SaCraMento"}]) == "Sacramento"
+
+
+
+
+
 
 #Still in progress
 def test_get_stories(): 
